1cc95870ee4429f51391952225f6a4729eb2f349
Max Schipper <max.schipper1@gmx.de>
2025-02-18 19:48:16 +0100

    chore(fish): clean up toggle_script function

--- fish/functions/toggle_script.fish
+++ fish/functions/toggle_script.fish
@@ -1,20 +1,19 @@
-function toggle
+function toggle_script
+    # toggles common boolean values used in config files
+    # if no match was found it returns the input back without modification
 
     set line (cat) # Read the entire line from stdin
 
-    # Match the part of the line before the value (the '=' and the possible space after it)
-    # set pre (string match -r '^=\s*' -- $line)
-
-    # Get the value after the '=' sign, ignoring the possible space
-    # set value (string replace -r '^=\s*' '' -- $line)
 
 
     switch $line
+
         # true/false
         case true
             echo false
         case false
             echo true
+
 
             # yes/no
         case yes
@@ -22,17 +21,20 @@ function toggle
         case no
             echo yes
 
+
             # y/n
         case y
             echo n
         case n
             echo y
 
+
             # on/off
         case on
             echo off
         case off
             echo on
+
 
             # no match: just return it without modification
         case "*"
