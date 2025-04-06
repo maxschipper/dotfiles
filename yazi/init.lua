3fbf79098afc5c207764b9553b860ed8133a31b4
Max Schipper <max.schipper1@gmx.de>
2025-04-06 18:31:50 +0200

    chore(yazi): push updates

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,3 +1,61 @@
 require("full-border"):setup()
 require("git"):setup()
-require("starship"):setup({ config_file = "~/.config/starship/starship_yazi.toml" })
+-- require("starship"):setup({ config_file = "~/.config/starship/starship-nf.toml" })
+
+
+-- yatline config
+require("yatline"):setup({
+	show_background = false,
+
+	display_header_line = false,
+	display_status_line = true,
+
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	-- section_separator = { open = "", close = "" },
+	-- part_separator = { open = "", close = "" },
+	-- inverse_separator = { open = "", close = "" },
+
+
+	header_line = {
+		left = {
+			section_a = {
+        			-- {type = "line", custom = false, name = "tabs", params = {"left"}},
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+        			-- {type = "coloreds", custom = true, name = {{" 󰇥 ", "#3c3836"}}},
+							{type = "coloreds", custom = false, name = "count", params = "true"}, 
+			},
+			section_b = {
+			},
+			section_c = {
+        			-- {type = "coloreds", custom = false, name = "count"},
+			}
+		}
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		}
+	},
+})
