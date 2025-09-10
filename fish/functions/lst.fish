cdf111826719c24ee7ec4b1a92093c48ca4388ef
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-11 01:17:30 +0200

    fish: update lst function with more ignore globs

--- fish/functions/lst.fish
+++ fish/functions/lst.fish
@@ -1,7 +1,13 @@
 function lst
-    if test -z $argv
-        lsd -A --tree --depth 2
+    if test -z "$argv"
+        lsd -A --tree --depth 2 -I .git \
+            -I .jj \
+            -I __pycache__ \
+            -I node_modules
     else
-        lsd -A --tree --depth $argv
+        lsd -A --tree --depth $argv -I .git \
+            -I .jj \
+            -I __pycache__ \
+            -I node_modules
     end
 end
