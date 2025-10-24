b321856b449bbf71fe1e00f939cb723672c7d54e
Max S <150921823+maxschipper@users.noreply.github.com>
2025-10-25 01:26:41 +0200

    fuzzel: update

--- fuzzel/scripts/power-menu.sh
+++ fuzzel/scripts/power-menu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-result=$(echo -e "Lock\nReboot\nHibernate\nPower off" | fuzzel --dmenu --placeholder="POWER MENU" --hide-prompt -l 4)
+result=$(echo -e "Lock\nLog out\nReboot\nHibernate\nPower off" | fuzzel --dmenu --placeholder="POWER MENU" --hide-prompt -l 5)
 
 if [[ "${result}" = "Power off" ]]; then
   systemctl poweroff -i
@@ -9,4 +9,6 @@ elif [[ "${result}" = "Hibernate" ]]; then
   systemctl hibernate
 elif [[ "${result}" = "Reboot" ]]; then
   systemctl reboot
+elif [[ "${result}" = "Log out" ]]; then
+  loginctl terminate-user $USER
 fi
