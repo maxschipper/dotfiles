7a8d01b6e0f8b0d3aaf62d963c9a742dd93b0bc2
Max S <150921823+maxschipper@users.noreply.github.com>
2025-10-31 14:27:39 +0100

    fish: disable l being alias for nu shells ls

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,8 @@ if status is-interactive
     alias ll='lsd -AF --group-directories-first --blocks git --blocks name --blocks permission --blocks user --blocks group --blocks size --blocks date --date relative'
     alias l='lsd -AF1'
 
+    # abbr -a l ', nu -c ls'
+
     #### STARSHIP START
     # starship init fish | source
     # set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
